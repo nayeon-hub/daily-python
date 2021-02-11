248d6ff833e87a3a5d65786311b6546d850e256e
Park Na Yeon <within197@naver.com>
2021-02-11 23:39:31 +0900

    python

--- python100/12.py
+++ python100/12.py
@@ -1,11 +1,19 @@
 class Wizard():
     '''게임 능력치'''
-    def setdata(self,health,mana,armor):
-      
-    def mana(self):
-        return
+    def __init__(self,health,mana,armor):
+        self.health = health
+        self.mana = mana
+        self.armor = armor
 
-    def health(self):
-        return
-    def armor(self):
-        return
+    # def mana(self):
+    #     return self.mana
+    # def health(self):
+    #     return self.health
+    # def armor(self):
+    #     return self.armor
+    def attack(self):
+        print("파이어볼")
+
+x = Wizard(health = 545, mana = 210, armor = 10)
+print(x.health, x.mana, x.armor)
+x.attack()
