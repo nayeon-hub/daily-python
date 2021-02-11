67a9f0395603170dd84a22b7c453f826c8388a72
Park Na Yeon <within197@naver.com>
2021-02-11 23:15:43 +0900

    python

---
+++ python100/12.py
@@ -0,0 +1,11 @@
+class Wizard():
+    '''게임 능력치'''
+    def setdata(self,health,mana,armor):
+      
+    def mana(self):
+        return
+
+    def health(self):
+        return
+    def armor(self):
+        return
